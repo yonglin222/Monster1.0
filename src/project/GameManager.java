e58c8116644a9b3fe0f94cee84a68676d2055662
yonglin222 <dydfls0429@gmail.com>
2025-07-16 09:07:09 +0900

    Update GameManager.java

--- src/project/GameManager.java
+++ src/project/GameManager.java
@@ -2,6 +2,7 @@ package project;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GameManager extends Monster {
     public GameManager(String name, int hp, int attack, int defense) {
@@ -10,7 +11,7 @@ public class GameManager extends Monster {
 
     public static void main(String[] args) {
         // Monster 객체들을 저장할 ArrayList를 생성
-        List<Monster> monsterList = new ArrayList<>();
+        ArrayList<Monster> monsterList = new ArrayList<>();
         monsterList.add(new Monster("슬라임", 30, 8, 5));
         monsterList.add(new Monster("고블린", 50, 12, 4));
         monsterList.add(new Monster("오크", 80, 15, 7));
@@ -22,9 +23,66 @@ public class GameManager extends Monster {
         monsterList.add(new Monster("키메라", 200, 30, 20));
         monsterList.add(new Monster("드래곤", 300, 40, 30));
 
-        for ()
-    }
+        for (Monster m : monsterList) {
+            m.info();
+        }
+        // (도전 과제) 플레이어가 몬스터 목록에서 두 마리를 선택하여
+        // 싸움을 붙이는 로직을 구현해 보세요.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("0~9번중 첫번째 몬스터를 고르시오: ");
+        int first = scanner.nextInt();
 
+        System.out.println("0~9번중 두번째 몬스터를 고르시오: ");
+        int second = scanner.nextInt();
+
+        // first, second 가 0~9사이로 선택할수 있도록 만들기
+        if (first < 0 || second < 0 || first > 9 || second > 9)
+            System.out.println("잘못된 선택입니다.");
+
+        Monster monster1 = monsterList.get(first);
+        Monster monster2 = monsterList.get(second);
+
+        while(monster1.getHp() > 0 && monster2.getHp() > 0) {
+            int damage = monster1.getAttack() - monster2.getDefense();
+            if (damage <= 0) damage = 0;
+
+            int newHp = monster2.getHp() - damage;
+            monster2.setHp(newHp > 0 ? newHp : 0);
+
+            System.out.println(monster1.getName() + "의 기본공격! " + monster2.getName() + "는 데미지" + damage + "를 입었다 ");
+            System.out.println(monster2.getName() + " 남은체력 : " + monster2.getHp());
+            System.out.println();
+            if (monster2.getHp() <= 0) break;
+            try {
+                // 1000 밀리초 = 1초 동안 실행을 멈춥니다.
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // sleep 도중 방해를 받았을 때 처리할 코드를 여기에 작성할 수 있습니다.
+                // 지금은 비워두어도 괜찮습니다.
+                e.printStackTrace();
+            }
+
+            damage = monster2.getAttack() - monster1.getDefense();
+            if (damage <= 0) damage = 0;
+
+            newHp = monster1.getHp() - damage;
+            monster1.setHp(newHp > 0 ? newHp : 0);
+
+            System.out.println(monster2.getName() + "의 기본공격! " + monster1.getName() + "는 데미지" + damage + "를 입었다 ");
+            System.out.println(monster1.getName() + " 남은체력 : " + monster1.getHp());
+            System.out.println();
+            if (monster1.getHp() <= 0) break;
+
+            try {
+                // 1000 밀리초 = 1초 동안 실행을 멈춥니다.
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // sleep 도중 방해를 받았을 때 처리할 코드를 여기에 작성할 수 있습니다.
+                // 지금은 비워두어도 괜찮습니다.
+                e.printStackTrace();
+            }
+        }
+    }
 }
 //        Monster monster1 = new Monster("슬라임", 30, 8, 3);
 //        Monster monster2 = new Monster("오크", 40, 12, 5);
